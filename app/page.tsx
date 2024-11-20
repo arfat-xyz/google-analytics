d981aac8599c283ac67817a4b8e2c364f98ad4b0
unknown <arfatrahman08@gmail.com>
2024-11-20 13:37:41 +0600

    google analytics added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const MainPage = () => {
   return (
-    <div>NextJs with Google analytics</div>
-  )
-}
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-2">
+        <div className="">NextJs with Google analytics</div>
+        <span>By</span>
+        <Link
+          className="text-blue-500"
+          target="_"
+          href={`https://www.arfat.app/`}
+        >
+          Arfatur Rahman
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
